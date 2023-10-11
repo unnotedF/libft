c1741ccb3e4ef03ba12e1c8366e183844806162c
unnotedF <fabriciol.sousa@live.com>
2023-10-11 15:04:34 +0100

    x

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: flucas-d <fabriciol.sousa@live.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 16:29:40 by flucas-d          #+#    #+#             */
-/*   Updated: 2023/10/11 14:36:38 by flucas-d         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:03:09 by flucas-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	int	str_len;
 
-	i = 0;
-	while (s[i])
-		i++;
-	if (c == '\0')
-		return ((char *)&s[i]);
-	while (i >= 0)
+	str_len = ft_strlen(s);
+	while (str_len >= 0)
 	{
-		if (s[i] == c)
-			return ((char *)&s[i]);
-		i--;
+		if (s[str_len] == (char)c)
+			return (&((char *)s)[str_len]);
+		str_len--;
 	}
-	return (0);
+	if (!(char)c)
+		return (&((char *)s)[str_len]);
+	return (NULL);
 }
 /*
 int	main()
